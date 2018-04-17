4935bb59292c69f1cdea7c0a4d5a416c94a43382
Administrador <Administrador@172.16.17.113>
2018-04-17 14:29:30 -0600

    examen en proceso!!!

---
+++ src/exaprac2/KatherineMayen.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exaprac2;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Administrador
+ */
+public class KatherineMayen {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int op=0;
+        while(op!=3)
+        {
+            System.out.println("Seleccione");
+             System.out.println("1.....Agregar empleado");
+              System.out.println("2.....Calcular planilla");
+               System.out.println("1.....salir");
+            op=Integer.parseInt(JOptionPane.showInputDialog("Ingrese Opcion"));
+            
+            switch(op)
+            {
+                case 1:
+             double salarioHora;
+             double salarioHora = 0 ;
+              double canHorasTra = 0;
+              double salario;
+            double nombre;
+                    salario = canHorasTra*salarioHora;
+                    
+                 
+                    
+                    
+                
+            }
+            break;
+            {
+                case 2:
+                
+                double salario;
+                double renta =0.10;
+                double iss = 0.7;
+                double afp= 0.4;
+                
+                
+                
+                
+            }
+            break;
+            {
+                case 3:
+                
+                
+            }
+        }
+        
+    }
+    
+}
